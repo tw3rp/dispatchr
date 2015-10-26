df7f9cc6b7738fb600b171759d73367c7c4faffb
tw3rp <rahuldprabhu@gmail.com>
2015-10-25 23:44:14 -0700

    added mongolab

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 var conn = mongoose.connection;
-var uri="mongodb://heroku_vfp6w0c0:6ghh5u7fmejj6r1jjt5030vpk7@ds033143.mongolab.com:33143/heroku_vfp6w0c0";
+var uri="mongodb://heroku_vfp6w0c0:1q2w3e4r5t@ds033143.mongolab.com:33143/heroku_vfp6w0c0"
 //uncomment the below line if you have mongo running locally 
 // var uri="mongodb://localhost/marketapp"
 conn.on('error',console.error.bind(console,"mongoose connection error"));
