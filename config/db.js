044f8b8dec784fe60796698a5d61a88e4bbff245
tw3rp <rahuldprabhu@gmail.com>
2015-10-25 23:35:23 -0700

    added remote mongo

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 var mongoose = require("mongoose");
 var conn = mongoose.connection;
-var uri="mongodb://localhost/marketapp"
+var uri="mongodb://heroku_vfp6w0c0:6ghh5u7fmejj6r1jjt5030vpk7@ds033143.mongolab.com:33143/heroku_vfp6w0c0";
+//uncomment the below line if you have mongo running locally 
+// var uri="mongodb://localhost/marketapp"
 conn.on('error',console.error.bind(console,"mongoose connection error"));
 conn.once('open',function(){
   console.log("connected to "+ uri);
